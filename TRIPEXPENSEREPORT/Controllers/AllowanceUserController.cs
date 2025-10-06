5680154f6feaae2324aa2924dfe144878adcd7c8
sarit-t <sarit_t@contrologic.co.th>
2025-10-06 08:48:56 +0700

    update home page & add allowanceuser

--- TRIPEXPENSEREPORT/Controllers/AllowanceUserController.cs
+++ TRIPEXPENSEREPORT/Controllers/AllowanceUserController.cs
@@ -6,7 +6,17 @@ namespace TRIPEXPENSEREPORT.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // สมมติส่งข้อมูลไปยัง View (สามารถเปลี่ยนเป็นการดึงจากฐานข้อมูลได้)
+            var model = new List<object>
+            {
+                new { Date = "01/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" },
+                new { Date = "02/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" },
+                new { Date = "03/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" },
+                new { Date = "04/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" },
+                new { Date = "05/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" },
+                new { Date = "06/09/25", Time = "08:30-17:30", Status = "ดำเนินการ", JobNo = "J24-0157", Col1 = 100, Col2 = 50, Col3 = 80, Col4 = 100, Col5 = "-" }
+            };
+            return View(model);
         }
     }
 }
--- TRIPEXPENSEREPORT/Controllers/HomeController.cs
+++ TRIPEXPENSEREPORT/Controllers/HomeController.cs
@@ -8,5 +8,23 @@ namespace TRIPEXPENSEREPORT.Controllers
         {
             return View();
         }
+
+        public IActionResult PersonalCarUser()
+        {
+            // หน้าเป้าหมายสำหรับการสั่งจองรถ
+            return View();
+        }
+
+        public IActionResult CompanyCarUser()
+        {
+            // หน้าเป้าหมายสำหรับการขอใช้ยานพาหนะ
+            return View();
+        }
+
+        public IActionResult AllowanceUser()
+        {
+            // หน้าเป้าหมายสำหรับการเบิกเงิน
+            return View();
+        }
     }
 }

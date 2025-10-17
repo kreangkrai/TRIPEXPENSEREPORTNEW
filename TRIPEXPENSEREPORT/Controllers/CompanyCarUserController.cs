dafa61a8a107983a0f10ebe12eecfe734b0d5ebc
sarit-t <sarit_t@contrologic.co.th>
2025-10-17 09:28:54 +0700

    update CompanyCarUser

---
+++ TRIPEXPENSEREPORT/Controllers/CompanyCarUserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TRIPEXPENSEREPORT.Models;
+
+namespace TRIPEXPENSEREPORT.Controllers
+{
+    public class CompanyCarUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            // สมมติส่งข้อมูลไปยัง View (สามารถเปลี่ยนเป็นการดึงจากฐานข้อมูลได้)
+            var model = new List<CompanyModel>
+            {
+                new CompanyModel()
+                {
+                    date = DateTime.Now.Date,
+                    car_id = "CAR#18",
+                    driver = "Sarit T.",
+                    time_start = new TimeSpan(8,30,00),
+                    time_stop = new TimeSpan(17,30,00),
+                    location = "Home-THIP SUGAR SUKHOTHAI",
+                    fleet = 1000,
+                    cash = 500,
+                    ctbo = 0,
+                    exp = 0,
+                    pt = 0,
+                    mileage_start = 10000,
+                    mileage_stop = 10200,
+                    km = 200,
+                    job = "J23-0869",
+                    status = "Pending"
+                }
+            };
+            return View(model);
+        }
+    }
+}
